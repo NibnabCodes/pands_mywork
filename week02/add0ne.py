7ced78502bb4de1fe433e8448d56a3e3dc577fb9
Niamh Hogan <hniamhh1995@gmail.com>
2025-02-12 19:29:45 +0000

    week2

---
+++ week02/add0ne.py
@@ -0,0 +1,7 @@
+# add0ne.py
+# This program asks the user to input a number and then prints out the number + 1
+# author: Niamh Hogan
+
+number = int(input('please enter a number:'))
+newNumber = number + 1
+print (f'{number} plus one is {newNumber}')
---
+++ week02/hello2.py
@@ -0,0 +1,6 @@
+# hello2.py
+# This program asks the user to input their name and then prints out hello, user's name + nice to meet you
+# author: Niamh Hogan
+  
+name = input ("Enter your name:")
+print ('Hello '+ name + '\nNice to meet you!')
