d7658cd8bb7e5cc967c043a3d06d26cfded457d2
Niamh Hogan <hniamhh1995@gmail.com>
2025-02-12 20:01:28 +0000

    week2Extra

---
+++ week02/nameAndAge.py
@@ -0,0 +1,7 @@
+# nameAndAge.py
+# This program asks the user to input their name and age. It then prints out a message to the user with their name and age + a tab at the end of their name.
+# author: Niamh Hogan
+
+name = input ("Enter your name:")
+age = input ("Enter your age:")
+print ("Hello " + f"{name},\t" + " Your age is " + age)
