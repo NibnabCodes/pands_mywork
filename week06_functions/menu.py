dac524dd133c3b2c0b0cf35c8b0f1545bfab86c5
Niamh Hogan <hniamhh1995@gmail.com>
2025-03-13 16:41:52 +0000

    week6

---
+++ week06_functions/menu.py
@@ -0,0 +1,4 @@
+# menu.py
+# This program creates a function that prints out a menu of commands we can perform, ie add, view and quit. 
+# The function returns what the user chose. 
+# author: Niamh Hogan
